537562bd675c73722310f506c02f3b4175eb64f2
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-07-27 03:02:54 +0300

    rework

---
+++ build/precache-manifest.f7cb0cfb48ea76c49622e8941c54d1d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e41be85585c962277421601ca3b879f",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "f0ada2705b232669bb06",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.fe134cec.chunk.css"
+  },
+  {
+    "revision": "b344a8d630cbb2e12576",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.f4461d46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.f4461d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ada2705b232669bb06",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.59f05835.chunk.js"
+  },
+  {
+    "revision": "fbce3650f28ef3d563ad",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.01e5adcb.js"
+  }
+]);
